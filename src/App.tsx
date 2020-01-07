499190292f995b6302fd5b52ca7629364784bbb5
Alpha Olomi <alphaolomi@gmail.com>
2020-01-08 02:13:24 +0300

    feat: add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Users from "./pages/Users";
+import Home from "./pages/Home";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import AppNav from "./components/AppNav";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <AppNav />
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Users.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+function Users() {
+    return <h2>Users</h2>;
+  }export default Users;
