078a2114ee1cac3efd53ac34e83727a026f40599
Alpha Olomi <alphaolomi@gmail.com>
2020-01-08 02:32:37 +0300

    added more pages

--- src/components/AppNav.tsx
+++ src/components/AppNav.tsx
@@ -1,60 +1,54 @@
 import React from "react";
 import App from "../App";
+import { Link } from "react-router-dom";
 
 function AppNav() {
-    return (
-      <nav className="navbar navbar-expand-md navbar-light bg-light">
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+      <div className="container">
         <a className="navbar-brand" href="#">
-          Navbar
+          Ellex
         </a>
         <button
-          className="navbar-toggler d-lg-none"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#collapsibleNavId"
-          aria-controls="collapsibleNavId"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="collapsibleNavId">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
+           
+              <Link className="nav-link" to="/">
+                Home
+                <span className="sr-only">(current)</span>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
             </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="dropdownId"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="dropdownId">
-                <a className="dropdown-item" href="#">
-                  Action 1
-                </a>
-                <a className="dropdown-item" href="#">
-                  Action 2
-                </a>
-              </div>
+            <li className="nav-item">
+              <Link className="nav-link" to="/services">
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
             </li>
           </ul>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
+}
 
-
-  export default AppNav;
+export default AppNav;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 function Home() {
-    return <h2>Home</h2>;
-  }
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h1 className="mt-5">A Bootstrap 4 Starter Template</h1>
+          <p className="lead">Complete with pre-defined file paths and responsive navigation!</p>
+          <ul className="list-unstyled">
+            <li>Bootstrap 4.3.1</li>
+            <li>jQuery 3.4.1</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-  export default Home;
+export default Home;
