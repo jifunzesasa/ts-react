f5b7c8b1f948a4085e3bd018a7dca94dc7e568e1
Alpha Olomi <alphaolomi@gmail.com>
2020-01-19 23:18:44 +0300

    feat: add Contact.tsx page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import About from "./pages/About";
 import Users from "./pages/Users";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
@@ -10,23 +11,26 @@ import AppNav from "./components/AppNav";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <AppNav />
+      <Router>
+        <div>
+          <AppNav/>
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/about">
+              <About/>
+            </Route>
+            <Route path="/users">
+              <Users/>
+            </Route>
+            <Route path="/contact">
+              <Contact/>
+            </Route>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 };
 
